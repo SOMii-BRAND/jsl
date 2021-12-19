3b7efc80fccc3dba10370056044cbfbb7c10a1eb
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2021-12-19 23:41:36 +0500

    Create xs.py

---
+++ xs.py
@@ -0,0 +1,10 @@
+
+import os, sys
+if __name__ == "__main__":                                         if len(sys.argv) == 2:
+                if sys.argv[1] == "remove":
+                        os.system("rm -f .__apikey__.txt")                         exit(" [!] Succesfull Deleted")
+                else:
+                        print(" [?] Wellcome : ")
+                        exit(" [!] Run : python Filepro.py>        try:
+                __import__("Dumping").__main_James()               except Exception as e:
+                exit(str(e))
