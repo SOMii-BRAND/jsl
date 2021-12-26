7f71c0690e11c3a3ff02a45a545909cf9d1ca92c
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2021-12-26 20:17:15 +0500

    Add files via upload

---
+++ jssl.py
@@ -0,0 +1,14 @@
+import os, sys
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "remove":
+			os.system("rm -f .__apikey__.txt")
+			exit(" [!] Succesfull Deleted")
+		else:
+			print(" [?] Wellcome : ")
+			exit(" [!] Run : python jssl.py remove")
+	try:
+		__import__("old").__main_Somi()
+	except Exception as e:
+		exit(str(e))
+ 
