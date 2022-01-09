9dba60ae360d977289482b56bb83a03003158355
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-01-09 15:14:35 +0500

    Update jsl.py

--- jsl.py
+++ jsl.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
 			print(" [?] Wellcome : ")
 			exit(" [!] Run : python Jsl.py remove")
 	try:
-		__import__("main").__main_Somi()
+		__import__("es").__main_Somi()
 	except Exception as e:
 		exit(str(e))
