0c0b5ddc0c32710096b03bf7be0c5ef011f11666
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-01-26 17:21:19 +0500

    Update and rename nm.py to jsl.py

--- jsl.py
+++ jsl.py
@@ -7,8 +7,8 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from ig import ___menu___
-    ___menu___()
+    from xjsl import somi_code
+    somi_code()
 elif bit == '32bit':
-    from ig import ___menu___
-    ___menu___()
+    from xjsl import ___menu___
+    somi_code()
