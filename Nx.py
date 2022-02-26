0b50bd118b7b201e5dfa022528f92fc525beb0e1
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-02-26 18:50:28 +0500

    Create Nx.py

---
+++ Nx.py
@@ -0,0 +1,15 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+ 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from nx import __start__
+    __start__()
+elif bit == '32bit':
+    from nx import __start__
+    __start__()
+ 
