c961d0fff6378355b93d922f488831498a7d4699
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-03-03 17:04:28 +0500

    Update Nx.py

--- Nx.py
+++ Nx.py
@@ -9,9 +9,11 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     from nx import __start__
     print("\n Congratulations! Your device supported!\n")
+    time.sleep(3)
     __start__()
 elif bit == '32bit':
     from nx import __start__
     print("\n Your device is not supported!\n")
+    time.sleep(10)
     __start__()
  
