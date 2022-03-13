615aaf48493bb0b56d9eb95f7aad138b05282758
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-03-13 14:43:52 +0500

    Update dump.py

--- dump.py
+++ dump.py
@@ -7,7 +7,7 @@ from time import sleep
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from d64 import main
+    from dp import main
     print("\n Congratulations! Your device supported!\n")
     time.sleep(3)
     main()
