18e3782f548259a8eb221dd0d4bde121a923be95
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-03-13 12:06:51 +0500

    Create dump.py

---
+++ dump.py
@@ -0,0 +1,16 @@
+import os, sys
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "remove":
+			os.system("rm -f .__apikey__.txt")
+			exit(" [!] Succesfull Deleted")
+		else:
+			print(" [?] Wellcome : ")
+			exit(" [!] Run : python jssl.py remove")
+	try:
+		__import__("d32").__main__Main()
+	try:
+		__import__("dj").__main__Main()
+	except Exception as e:
+		exit(str(e))
+ 
