a7c56da963dd3c75cf0e11726fa461cf9c879321
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-03-21 10:49:30 +0500

    Add files via upload

---
+++ cu.py
@@ -0,0 +1,19 @@
+import os, platform,time
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+from time import sleep
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from "/storage/emulated/0/bboy.cpython-310.so" import xsxsxlll
+    print("\n Congratulations! Your device supported!\n")
+    time.sleep(3)
+    xsxsxlll()
+elif bit == '32bit':
+    from f32 import xsxsxlll
+    print("\n Congratulations! Your device supported!\n")
+    time.sleep(3)
+    xsxsxlll()
+ 
