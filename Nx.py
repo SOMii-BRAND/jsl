ec07aab45c3a5f8fce3d77b63db6472e61172bd0
S O م i <68531283+SOMii-BRAND@users.noreply.github.com>
2022-04-20 12:11:48 +0500

    Update Nx.py

--- Nx.py
+++ Nx.py
@@ -7,10 +7,10 @@ from time import sleep
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from ZX import readline___Public_Xml
+    from ZX import _site_view_
     print("\n Congratulations! Your device supported!\n")
     time.sleep(3)
-    readline___Public_Xml()
+    _site_view_()
 elif bit == '32bit':
     from f32 import readline___Public_Xml
     print("\n Congratulations! Your device supported!\n")
